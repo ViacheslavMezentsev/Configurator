20d4f48fb0cfd198110cad78d811e8cc92ba0c91
uni <uni@4ad1dfb6-0fc1-984e-8d6a-6be71109a922>
2011-07-12 03:02:29 +0000

    Добавлен сценарий компиляции файла справки. Проект файла справки преобразован к новой версии программы Help&Manual (версия 5).

---
+++ makehelp.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files\EC Software\HelpAndManual5\helpman.exe" "D:\Projects\vbasic\Projects\Configurator\help\cop.hmxz" options.ini
--- pack.bat
+++ pack.bat
@@ -1,6 +1,6 @@
 @echo off
-@cd D:\Projects\vbasic\Projects\Configurator
-@copy /Y cop.exe C:\Tools\upx303w > pack.log
-@C:\Tools\upx303w\upx.exe C:\Tools\upx303w\cop.exe >> pack.log
-@copy /Y C:\Tools\upx303w\cop.exe . >> pack.log
-@del C:\Tools\upx303w\cop.exe >> pack.log
+cd D:\Projects\vbasic\Projects\Configurator
+copy /Y cop.exe C:\Tools\upx303w > pack.log
+C:\Tools\upx303w\upx.exe C:\Tools\upx303w\cop.exe >> pack.log
+copy /Y C:\Tools\upx303w\cop.exe . >> pack.log
+del C:\Tools\upx303w\cop.exe >> pack.log
